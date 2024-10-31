fe1e76bfc16128718710b67e80e7844eb2591e36
ZAIN-UL-ABEDIN <zainhanif2902@gmail.com>
2024-10-31 15:53:39 +0500

    setup project

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col min-h-screen min-w-screen items-center justify-center text-center">
+        <h1 className="animate-bounce text-5xl text-blue-800 font-bold">
+          React App
+        </h1>
+        <Button>Submit</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
